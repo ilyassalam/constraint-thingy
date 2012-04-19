94e97701ce5c8dacec3837444154d9f7f6fbebff
leifaffles <leifaffles@gmail.com>
2012-04-19 23:12:42 +0000

    Added missing docstrings for AABB, Vector2, and Interval structs

--- Intervals/Vector2.cs
+++ Intervals/Vector2.cs
@@ -5,40 +5,68 @@ namespace Intervals
     /// </summary>
     public struct Vector2
     {
+        /// <summary>
+        /// The X coordinate of the vector
+        /// </summary>
         public float X;
+
+        /// <summary>
+        /// The Y coordinate of the vector
+        /// </summary>
         public float Y;
 
+        /// <summary>
+        /// Creates a new Vector2 struct from the provided X and Y coordinates
+        /// </summary>
         public Vector2(float x, float y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// Adds two vectors
+        /// </summary>
         public static Vector2 Add(Vector2 a, Vector2 b)
         {
             return new Vector2(a.X + b.X, a.Y + b.Y);
         }
 
+        /// <summary>
+        /// Subtracts vector <paramref name="a"/> from vector <paramref name="b"/>
+        /// </summary>
         public static Vector2 Subtract(Vector2 a, Vector2 b)
         {
             return new Vector2(a.X - b.X, a.Y - b.Y);
         }
 
+        /// <summary>
+        /// Adds two vectors
+        /// </summary>
         public static Vector2 operator +(Vector2 a, Vector2 b)
         {
             return Add(a, b);
         }
 
+        /// <summary>
+        /// Subtracts vector <paramref name="a"/> from vector <paramref name="b"/>
+        /// </summary>
         public static Vector2 operator -(Vector2 a, Vector2 b)
         {
             return Subtract(a, b);
         }
 
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
         public static Vector2 Multiply(Vector2 vector, float scalar)
         {
             return new Vector2(vector.X * scalar, vector.Y * scalar);
         }
 
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
         public static Vector2 operator *(Vector2 vector, float scalar)
         {
             return Multiply(vector, scalar);

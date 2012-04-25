e8ed5572626594293e4ec102d3bb184b5f5977d8
leifaffles@gmail.com <leifaffles@gmail.com@123a2da4-a9bc-5bf5-6c44-090abbd7cd8c>
2012-04-25 20:09:08 +0000

    Started the addition constraint and interval variable. Graph canvas now properly cleans up text.

---
+++ trunk/ConstraintThingy/AdditionConstraint.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Intervals;
+
+namespace ConstraintThingy
+{
+    public class AdditionConstraint : Constraint<IntervalVariable>
+    {
+        /// <summary>
+        /// Constrains <paramref name="sum"/> = <paramref name="a"/> + <paramref name="b"/>
+        /// </summary>
+        public AdditionConstraint(IntervalVariable sum, IntervalVariable a, IntervalVariable b)
+            : base(new[] { sum, a, b })
+        {
+        }
+
+        private IntervalVariable Sum { get { return Variables[0]; } }
+        private IntervalVariable A { get { return Variables[1]; } }
+        private IntervalVariable B { get { return Variables[2]; } }
+
+        /// <summary>
+        /// Called when one of the variables participating in the constraint is narrowed.
+        /// </summary>
+        public override void Narrowed(Variable narrowedVariable)
+        {
+            Interval intersection = NarrowConstraint(narrowedVariable);
+
+            if (intersection.IsEmpty) throw new Failure("Empty interval.");
+        }
+
+        // narrows the value of a variable involved in the constraint
+        private Interval NarrowConstraint(Variable narrowedVariable)
+        {
+            Interval intersection;
+
+            // Sum = A + B
+            if (narrowedVariable == Sum)
+            {
+                Interval sum = A.Value + B.Value;
+                intersection = Interval.Intersection(sum, Sum.Value);
+            }
+            // A = Sum - B
+            else if (narrowedVariable == A)
+            {
+                Interval a = Sum.Value - B.Value;
+                intersection = Interval.Intersection(a, A.Value);
+            }
+            // B = Sum - A
+            else if (narrowedVariable == B)
+            {
+                Interval b = Sum.Value - A.Value;
+                intersection = Interval.Intersection(b, B.Value);
+            }
+            else
+            {
+                throw new InvalidOperationException("Unknown variable.");
+            }
+
+            return intersection;
+        }
+
+        /// <summary>
+        /// Tries to narrow the variable based on the constraint.  Returns false if variable cannot be narrowed.
+        /// </summary>
+        public override void UpdateVariable(IntervalVariable var)
+        {
+            Interval intersection = NarrowConstraint(var);
+
+            if (intersection.IsEmpty) throw new Failure("Empty interval.");
+
+            var.Value = intersection;
+        }
+    }
+}
---
+++ trunk/ConstraintThingy/IntervalVariable.cs
@@ -0,0 +1,22 @@
+﻿using Intervals;
+
+namespace ConstraintThingy
+{
+    public class IntervalVariable : Variable<Interval>
+    {
+        public IntervalVariable(string name, Interval initialValue)
+            : base(name, initialValue)
+        {
+        }
+
+        public override bool IsUnique
+        {
+            get { return Value.IsUnique; }
+        }
+
+        public override bool IsEmpty
+        {
+            get { return Value.IsEmpty; }
+        }
+    }
+}
